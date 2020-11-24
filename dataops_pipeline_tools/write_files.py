1239827ce3e880e59b1b9375ae16f950a38c46d9
ShawonC <shawon@puppet.com>
2020-11-24 15:57:52 -0600

    adding file stream function to library

--- dataops_pipeline_tools/write_files.py
+++ dataops_pipeline_tools/write_files.py
@@ -1,6 +1,8 @@
 import jsonlines
 import json
 
+from smart_open import open as smartopen
+
 def write_to_file(data: dict, outfile: str, schema: dict) -> None:
     """Write data to a json file in either newline-delimited or pretty json format.
     Write schema to file in json format.
@@ -18,3 +20,25 @@ def write_to_file(data: dict, outfile: str, schema: dict) -> None:
 
     with open(f"{outfile}.bqschema.json", "w") as writer:
         json.dump(schema, writer, indent=2, sort_keys=True)
+
+def stream_to_gcs(data: dict, bucket: str, outfile: str, schema: dict, date: str) -> None:
+    """Stream data in newline-delimited json format to Google Cloud Storage.
+    Stream schema to GCS in json format.
+
+    Args:
+        data: The data to write to a file
+        outfile: The path to write the file to
+        schema: The schema dictionary to write to a schema file
+    Returns:
+        None
+    """
+    with smartopen(
+        f"gs://{bucket}/puppet_download_logs/{outfile}/{outfile}-{date}.ndjson", "w"
+    ) as file_out:
+        for item in data:
+            file_out.write(f"{json.dumps(item)}\n")
+
+    with smartopen(
+        f"gs://{bucket}/puppet_download_logs/{outfile}/{outfile}.schema.json", "w"
+    ) as file_out:
+        file_out.write(json.dumps(schema))
